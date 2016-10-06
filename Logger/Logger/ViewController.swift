0ca797d17fec2b77d44d288f4ceb5c6d711e487c
duharikr <h.duraiamuthan@gmail.com>
2016-10-06 23:11:57 +0530

    adding wkwebview for loading url

--- Logger/Logger/ViewController.swift
+++ Logger/Logger/ViewController.swift
@@ -8,12 +8,32 @@
 
 import UIKit
 import WebKit
+import WBWebViewConsole
 
 class ViewController: UIViewController {
 
+    var webView: WBWKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView = WBWKWebView(frame: self.view.bounds)
+        webView.JSBridge.interfaceName="WKWebViewBridge"
+        webView.JSBridge.readyEventName="WKWebViewBridgeReady"
+        webView.JSBridge.invokeScheme="wkwebview-bridge://invoke"
+        webView.loadRequest(getUrlRequestForLan())
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func getUrlRequestForLan() -> NSURLRequest{
+        let url = NSURL(string: "https://www.google.com")!
+        let isReachable = NetworkManager.sharedManager().isInternetReachable()
+        let urlRequestCache:NSURLRequest!
+        if isReachable != 0  {
+            urlRequestCache = NSURLRequest(URL: url, cachePolicy: NSURLRequestCachePolicy.UseProtocolCachePolicy, timeoutInterval: 60)
+        }else{
+            urlRequestCache = NSURLRequest(URL: url, cachePolicy: NSURLRequestCachePolicy.ReturnCacheDataElseLoad, timeoutInterval: 60)
+        }
+        return urlRequestCache
     }
 
     override func didReceiveMemoryWarning() {
