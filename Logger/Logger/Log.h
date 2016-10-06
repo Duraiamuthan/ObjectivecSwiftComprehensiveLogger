ecfe0da7aecc76565cb9a481d9105b7f3e94e77d
duharikr <h.duraiamuthan@gmail.com>
2016-10-06 22:46:38 +0530

    Adding Objective c logger

---
+++ Logger/Logger/Log.h
@@ -0,0 +1,15 @@
+//
+//  Log.m
+//  OmniclientSwift
+//
+//  Created by Durai on 04/02/16.
+//  Copyright © 2016 Ericsson Research. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define NSLog(args...) _Log(@"DEBUG ", __FILE__,__LINE__,__PRETTY_FUNCTION__,args);
+//@class DeviceManager;
+@interface Log : NSObject
+void _Log(NSString *prefix, const char *file, int lineNumber, const char *funcName, NSString *format,...);
+@end
---
+++ Logger/Logger/Log.m
@@ -0,0 +1,25 @@
+//
+//  Log.m
+//  OmniclientSwift
+//
+//  Created by Durai on 04/02/16.
+//  Copyright © 2016 Ericsson Research. All rights reserved.
+//
+
+#import "Log.h"
+#import "Logger-Swift.h"
+
+@implementation Log
+
+void _Log(NSString *prefix, const char *file, int lineNumber, const char *funcName, NSString *format,...) {
+    va_list ap;
+    va_start (ap, format);
+    format = [format stringByAppendingString:@"\n"];
+    NSString *msg = [[NSString alloc] initWithFormat:[NSString stringWithFormat:@"%@",format] arguments:ap];
+    va_end (ap);
+    fprintf(stderr,"%s%50s:%3d - %s",[prefix UTF8String], funcName, lineNumber, [msg UTF8String]);
+    DeveloperConsoleManager *consoleManagerObj=[DeveloperConsoleManager sharedInstance];
+    [consoleManagerObj writeObcLog:msg];
+}
+
+@end
