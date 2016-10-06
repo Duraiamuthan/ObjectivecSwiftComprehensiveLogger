5197e890fac5d022eda978f8cd1ca5c1dccd93ea
duharikr <h.duraiamuthan@gmail.com>
2016-10-06 22:35:49 +0530

    WBWebViewConsole pod integration
    WBWebViewConsole pod integration

---
+++ Logger/Pods/Target Support Files/WBWebViewConsole/WBWebViewConsole-umbrella.h
@@ -0,0 +1,31 @@
+#import <UIKit/UIKit.h>
+
+#import "WBWebDebugConsoleViewController.h"
+#import "WBWebViewConsoleUserPromptCompletionController.h"
+#import "WBWebViewConsoleUserPromptCompletionDatasource.h"
+#import "WBJSBridgeActionPrivateConsoleLog.h"
+#import "WBWebViewConsole.h"
+#import "WBWebViewConsoleInputHistoryEntry.h"
+#import "WBWebViewConsoleMessage.h"
+#import "NSDictionary+WBTTypeCast.h"
+#import "NSObject+WBJSONKit.h"
+#import "UIColor+WBTHelpers.h"
+#import "UIDevice+WBTHelpers.h"
+#import "UIScrollView+WBTUtilities.h"
+#import "UIView+WBTSizes.h"
+#import "WBKeyboardObserver.h"
+#import "WBTextView.h"
+#import "WBTTypeCastUtil.h"
+#import "WBJSBridgeAction.h"
+#import "WBJSBridgeMessage.h"
+#import "WBWebViewJSBridge.h"
+#import "WBWebView.h"
+#import "WBWebViewUserScript.h"
+#import "WBWebViewConsoleDefines.h"
+#import "WBWebViewConsoleInputView.h"
+#import "WBWebViewConsoleInputViewActionButton.h"
+#import "WBWebViewConsoleMessageCell.h"
+
+FOUNDATION_EXPORT double WBWebViewConsoleVersionNumber;
+FOUNDATION_EXPORT const unsigned char WBWebViewConsoleVersionString[];
+
